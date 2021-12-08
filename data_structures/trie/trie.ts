a9b617246f0c0e52cbf901f05fed3fce5892eef5
Hrach2003 <hrachya.hovakimyan.03@mail.ru>
2021-12-08 15:07:36 +0400

    feat(trie): implement trie

---
+++ data_structures/trie/trie.ts
@@ -0,0 +1,52 @@
+interface TrieNode extends Record<string, TrieNode> {}
+
+export class Trie {
+  root: TrieNode;
+
+  constructor() {
+    this.root = {
+      "*": {},
+    };
+  }
+
+  addWord(word: string) {
+    const charList = Array.from(word);
+
+    let trie = this.root["*"];
+
+    for (let i = 0; i < charList.length; i++) {
+      const char = charList[i];
+      if (char in trie) {
+        trie = trie[char];
+      } else {
+        this.addUniqueWord(trie, charList.slice(i));
+        break;
+      }
+    }
+  }
+
+  private addUniqueWord(trieNode: TrieNode, charList: string[]) {
+    let currentTrie = trieNode;
+
+    charList.forEach((char) => {
+      currentTrie[char] = {};
+      currentTrie = currentTrie[char];
+    });
+
+    currentTrie["*"] = {};
+  }
+
+  getCompletion(incompleteWord: string) {
+    const charList = Array.from(incompleteWord);
+    let trie = this.root["*"];
+
+    for (let i = 0; i < charList.length; i++) {
+      const char = charList[i];
+      if (char in trie) {
+        trie = trie[char];
+      } else return [];
+    }
+
+    return Object.keys(trie);
+  }
+}
